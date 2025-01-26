4e91b84907f1391cd1273daf4cea39ac153f245e
gopakumar-k-a <gopak9145@gmail.com>
2025-01-26 15:15:20 +0530

    added vercel.json

--- src/components/alert/CustomAlertDialogue.tsx
+++ src/components/alert/CustomAlertDialogue.tsx
@@ -1,30 +1,30 @@
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-
-
-export function CustomAlertDialogue({ isOpen, onClose, title, description,onContinue }:{isOpen:boolean, onClose:()=>void, title:string, description:string,onContinue:()=>void }) {
-  return (
-    <AlertDialog open={isOpen} onOpenChange={onClose} >
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>{title}</AlertDialogTitle>
-          <AlertDialogDescription>
-          {description}
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-}
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+  } from "@/components/ui/alert-dialog";
+  
+  
+  export function CustomAlertDialogue({ isOpen, onClose, title, description,onContinue }:{isOpen:boolean, onClose:()=>void, title:string, description:string,onContinue:()=>void }) {
+    return (
+      <AlertDialog open={isOpen} onOpenChange={onClose} >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{title}</AlertDialogTitle>
+            <AlertDialogDescription>
+            {description}
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
+  }
